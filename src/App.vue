518d56781fa6efaff361b759498a026d3938eec7
Zariy7 <nazariy.hereha@gmail.com>
2024-01-15 09:03:07 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+import { store } from './store.js';
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+  components:{
+    AppHeader,
+  },
+  data (){
+    return{
+        store,
+    }
+  }
+}
+</script>
+
+<template>
+  <AppHeader />
+</template>
+
+<style lang="scss">
+@use './styles/generals.scss';
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,19 @@
+<script>
+import { store } from '../store.js';
+
+export default {
+  name: 'AppHeader',
+  data (){
+    return{
+        store,
+    }
+  }
+}
+</script>
+
+<template>
+    <h1>Header</h1>
+</template>
+
+<style lang="scss" scoped>
+</style>
